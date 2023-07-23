2c8daefa454dc140a61b2099be91ead813d01a56
imane omari <imane3.omari@gmail.com>
2023-07-23 23:02:18 +0100

    Completed assignment

---
+++ index.js
@@ -0,0 +1,15 @@
+// Function expression called divide
+function divide(a, b) {
+    return a / b;
+  }
+  
+  // Arrow function called square
+  const square = (x) => {
+    return x * x;
+  };
+  
+  // Arrow function called add
+  const add = (a, b) => {
+    return a + b;
+  };
+  
